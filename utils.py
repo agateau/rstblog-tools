f951eeb176f148c66b2ac50506a2c8bf48c632db
Aurélien Gâteau <mail@agateau.com>
2015-12-21 08:16:31 +0100

    Rewrite rstblog-deploy in Python

--- utils.py
+++ utils.py
@@ -4,3 +4,10 @@ import sys
 def die(msg):
     print(msg, file=sys.stderr)
     sys.exit(1)
+
+
+def confirm(prompt):
+    answer = input(prompt + ' [yN] ')
+    if not answer:
+        return False
+    return answer[0].lower() == 'y'

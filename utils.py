377b75a14afbc4223d1bcb17c06c74188915ac1c
Aurelien Gateau <mail@agateau.com>
2020-12-12 16:53:38 +0100

    Make articles public by default

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import sys
+import time
+
+from datetime import datetime
 
 BREAK = '<!-- break -->'
+
+
+def tzstring():
+    delta = time.timezone
+    dt = datetime.utcfromtimestamp(abs(delta))
+    txt = dt.strftime('%H:%M')
+    if delta > 0:
+        return '-' + txt
+    else:
+        return '+' + txt
+
+
+def get_date():
+    now = datetime.now()
+    return now.strftime('%Y-%m-%d %H:%M:%S') + ' ' + tzstring()
 
 
 def die(msg):
